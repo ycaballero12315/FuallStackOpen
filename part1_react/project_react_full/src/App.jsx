6f1910ae0062805db9319a304e783b57f64bcfbf
YoeDev <178677708+YoeDev@users.noreply.github.com>
2025-05-09 09:48:28 -0400

    react completo

--- part1_react/project_react_full/src/App.jsx
+++ part1_react/project_react_full/src/App.jsx
@@ -40,6 +40,105 @@ const Total = ({ course }) => {
   return <p>El total de la suma de ejercicios es: {sum}</p>;
 };
 
+const Edad = ({ birthYear, birthMonth, birthDay }) => {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  let age = currentYear - birthYear;
+
+  const hasBirthayPassed =
+    date.getDay() >= birthMonth || date.getDay() > birthDay;
+
+  if (!hasBirthayPassed) {
+    age--;
+  }
+  return (
+    <p>
+      usted tiene: {age} annos. nacio el {birthDay}/{birthMonth}/{birthYear}
+    </p>
+  );
+};
+
+const Counter = () => {
+  let [count, setCounter] = useState(0);
+  return (
+    <div>
+      <p>usted a clikeado {count} veces...</p>
+      <button onClick={() => setCounter(count + 1)}>ClicMe</button>
+      <br />
+      <button onClick={() => setCounter((count = 0))}>Resset</button>
+    </div>
+  );
+};
+
+const MostrarTexto = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "Ocultar" : "Mostrar"} mensaje
+      </button>
+      {visible && "!Hello soy el mensaje oculto"}
+    </div>
+  );
+};
+
+const Formulario = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Escibe tu nomble please: "
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <p>Su nombre es {name}</p>
+    </div>
+  );
+};
+
+const Semaforo = () => {
+  const [colorActivo, setColor] = useState("");
+  const [colorFondo, setColorFondo] = useState({});
+
+  return (
+    <div>
+      <h2>Semaforo</h2>
+      <button
+        onClick={() => {
+          setColor("Rojo");
+          setColorFondo({ backgroundColor: "red" });
+        }}
+      >
+        Rojo
+      </button>
+      <button
+        onClick={() => {
+          setColor("Amarillo");
+          setColorFondo({ backgroundColor: "yellow" });
+        }}
+      >
+        Amarillo
+      </button>
+      <button
+        onClick={() => {
+          setColor("Verde");
+          setColorFondo({ backgroundColor: "green" });
+        }}
+      >
+        Verde
+      </button>
+      {colorActivo && (
+        <p style={{ padding: "10px", color: "black", ...colorFondo }}>
+          El color activo es:
+          <strong>{colorActivo}</strong>
+        </p>
+      )}
+    </div>
+  );
+};
 const App = () => {
   return (
     <>
@@ -51,6 +150,14 @@ const App = () => {
         <br />
         <Total course={course}></Total>
       </div>
+      <div>
+        <Edad birthYear={1983} birthDay={31} birthMonth={12}></Edad>
+      </div>
+      <Counter></Counter>
+      <MostrarTexto></MostrarTexto>
+      <br />
+      <Formulario></Formulario>
+      <Semaforo></Semaforo>
     </>
   );
 };
